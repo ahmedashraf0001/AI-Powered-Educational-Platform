cbe998ea2691ef60202036b7ee246a9205e34e4d
ahmedashraf0001 <ashrafalaslogy90@gmail.com>
2026-01-26 21:16:33 +0200

    init

--- AIEduPlatform/AIEduPlatform.Application/obj/Debug/net10.0/AIEduPlatform.Application.AssemblyInfo.cs
+++ AIEduPlatform/AIEduPlatform.Application/obj/Debug/net10.0/AIEduPlatform.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIEduPlatform.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ace511f4d28a7c9086faba8b56f4db0b79dc70f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIEduPlatform.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIEduPlatform.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
